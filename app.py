ff8396fe1c4c1576ef5e5b098853d3d77f6871a4
Ajay2307 <ajay.prodevans@gmail.com>
2019-08-20 09:54:29 +0000

    changed title

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ colors = {
 }
 
 app.layout = html.Div(children=[
-    html.H1(children='Welcome to Prodevans',style={
+    html.H1(children='Welcome to Prodevans Iventura',style={
         'textAlign': 'center',
         'color': colors['text']}),
     dcc.Graph(id='project1',
