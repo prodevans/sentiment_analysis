4e7d48187c75118714dcbd4f9c9365889ce42574
Ajay Rudresh <47887498+Ajay2307@users.noreply.github.com>
2019-09-11 15:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ colors = {
 }
 
 app.layout = html.Div(children=[
-    html.H1(children='Welcome to Iventura - The Data Detective',style={
+    html.H1(children='Welcome to Iventura - Data Detective',style={
         'textAlign': 'center',
         'color': colors['text']}),
     dcc.Graph(id='project1',
